65a063afc1d575e18f0810f54250304d5b323f24
hurlingfan11 <39068491+hurlingfan11@users.noreply.github.com>
2018-11-04 19:46:01 +0000

    Food Item View + code stubs
    Gradle change

--- Foodrater/app/src/main/java/com/clancy/conor/foodrater/MainActivity.java
+++ Foodrater/app/src/main/java/com/clancy/conor/foodrater/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -17,6 +19,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view); // name as per content main
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setHasFixedSize(true);
+
+        //TODO: Connect the RecycleView to our NameAdapter
+        final FoodAdapter foodAdapter = new FoodAdapter();
+        recyclerView.setAdapter(foodAdapter);
+
+        foodAdapter:addFood();
+        foodAdapter:addFood();
+        foodAdapter:addFood();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
